b77ff82c699305073a771ca4aab5caa70c6c7966
BenFrancka <benfrancka@gmail.com>
2021-06-24 13:11:31 -0700

    adds setup for testing files

---
+++ __tests__/munge.test.js
@@ -0,0 +1,12 @@
+const { mungeLocation, mungeWeather } = require('../lib/munge.js');
+
+require('dotenv').config();
+
+
+describe('munge functions', () => {
+
+  test('takes in location data and turns it into a better object', async() => {
+    
+    
+  });
+});
